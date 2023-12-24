15deeebe63f2f46ff90bdc8ffe2a1bf3ab83a36c
shubin.andrej <haxapaktepe@gmail.com>
2023-12-24 10:41:26 +0400

    add contacts? privacyPolicy

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -34,4 +34,55 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     })
   })
+
+  var points = [
+    [
+      '<div class="map-baloon"><p>119311, г. Москва, Тц Универ Сити, площадь Джавахарлала Неру, 1</p></div>',
+      '55.693297',
+      '37.533984',
+    ],
+  ]
+
+  if (document.querySelector('.map')) {
+    ymaps?.ready(function () {
+      var myCollection = new ymaps.GeoObjectCollection()
+
+      myMap = new ymaps.Map('mapYandex', {
+        center: [55.693297, 37.533984],
+        zoom: 16,
+        controls: ['zoomControl', 'geolocationControl', 'trafficControl'],
+      })
+
+      if (innerWidth < 1024) {
+        myMap.behaviors.disable('scrollZoom')
+        myMap.behaviors.disable('drag')
+      }
+
+      for (i = 0; i < points.length; i++) {
+        var myPlacemark = new ymaps.Placemark(
+          [points[i][1], points[i][2]],
+          {
+            balloonContent: points[i][0],
+          },
+          {
+            iconLayout: 'default#image',
+            // iconImageHref: '../img/icons/loca.svg',
+            // iconImageSize: [48, 48],
+          }
+        )
+        myCollection.add(myPlacemark)
+        myMap.geoObjects.add(myPlacemark)
+
+        myMap.events.add('click', function (e) {
+          myMap.balloon.close()
+        })
+      }
+
+      myMap.geoObjects.add(myCollection)
+
+      myPlacemark.events.add('click', function (event) {
+        event.preventDefault()
+      })
+    })
+  }
 })
